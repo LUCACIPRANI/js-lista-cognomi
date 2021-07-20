cefaea77afb78df612cfefef4f164af6a8cce9c7
Luca <lucaciprani1@gmail.com>
2021-07-20 16:45:03 +0200

    Lista cognomi

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Esercizio Lista cognomi :
+
+var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+    console.log("Lista: " + cognomi);
+var cognomeInserito = prompt("Inserisci il tuo cognome ");
+
+
+// 1. chiedi all’utente il cognome;
+cognomi.push(cognomeInserito);
+
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’;
+console.log("Inserisco il cognome richiesto all'utente: " + cognomi);
+
+// 3. stampa la lista ordinata alfabeticamente;
+
+cognomi.sort();
+console.log("Ordine alfabetico: " + cognomi);
+
+for(var i = 0; i < cognomi.length; i++) {
+    document.getElementById("cognomi").innerHTML += "<li>" + cognomi[i] + "</li>";
+}
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova. 
+
+var idxOf = cognomi.indexOf(cognomeInserito) + 1;
+    console.log(idxOf);
+

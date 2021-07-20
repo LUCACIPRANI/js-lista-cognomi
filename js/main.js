891f442b7134b7eb3b55beb1c27e4856cc2985e3
Luca <lucaciprani1@gmail.com>
2021-07-20 17:31:54 +0200

    background

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Esercizio Lista cognomi :
 
+
 var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
     console.log("Lista: " + cognomi);
-var cognomeInserito = prompt("Inserisci il tuo cognome ");
+var cognomeInserito = prompt("Inserisci il tuo cognome "); 
 
-
-// 1. chiedi all’utente il cognome;
+//  1. chiedi all’utente il cognome;
 cognomi.push(cognomeInserito);
 
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’;
@@ -13,8 +13,13 @@ console.log("Inserisco il cognome richiesto all'utente: " + cognomi);
 
 // 3. stampa la lista ordinata alfabeticamente;
 
+for(var i = 0; i < cognomi.length; i++) {
+    cognomi[i] = cognomi[i].toUpperCase();
+}
+
 cognomi.sort();
 console.log("Ordine alfabetico: " + cognomi);
+
 
 for(var i = 0; i < cognomi.length; i++) {
     document.getElementById("cognomi").innerHTML += "<li>" + cognomi[i] + "</li>";
@@ -22,6 +27,6 @@ for(var i = 0; i < cognomi.length; i++) {
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova. 
 
-var idxOf = cognomi.indexOf(cognomeInserito) + 1;
-    console.log(idxOf);
+var idx = cognomi.indexOf(cognomeInserito) + 1;
+    console.log(idx);
 
